f4224a14a44871f08d604ebb89901175693b9a09
Winni Neessen <winfried.neessen@cleverbridge.com>
2021-06-23 23:08:31 +0200

    Better tab handling

--- Chrome/background.js
+++ Chrome/background.js
@@ -6,25 +6,32 @@
 chrome.runtime.onInstalled.addListener(function() {
     chrome.tabs.onUpdated.addListener(async (tabId, changeInfo, tabInfo) => {
         const isSoT = tabInfo.url.startsWith('https://www.seaofthieves.com/');
-        if(isSoT) {
-            chrome.action.enable(tabId)
-            chrome.action.setIcon({path: 'img/active_128.png', tabId: tabId})
-            chrome.cookies.get({url: 'https://www.seaofthieves.com', name: 'rat'}, async cookieInfo => {
-                if(cookieInfo.value !== '') {
-                    let responseCookie = {
-                        Value: cookieInfo.value,
-                        Expiration: Math.floor(cookieInfo.expirationDate)
-                    }
-                    let cookieJSON = JSON.stringify(responseCookie)
-                    let cookieBase64 = btoa(cookieJSON)
-                    chrome.storage.local.set({rat: cookieBase64}, function() {});
-                }
-            })
-        }
-        else {
-            chrome.action.disable(tabId)
-        }
+        if(isSoT) { EnableApp(tabInfo.tabId) }
+        else { DisableApp(tabInfo.tabId) }
     })
+    chrome.tabs.onActivated.addListener(async (tabInfo) => {
+        const isSoT = tabInfo.url.startsWith('https://www.seaofthieves.com/');
+        if(isSoT) { EnableApp(tabId) }
+        else { DisableApp(tabId) }
+    })
+
+    async function EnableApp(tabId) {
+        chrome.action.enable(tabId)
+        chrome.action.setIcon({path: 'img/active_128.png', tabId: tabId})
+        chrome.cookies.get({url: 'https://www.seaofthieves.com', name: 'rat'}, async cookieInfo => {
+            if(cookieInfo.value !== '') {
+                let responseCookie = {
+                    Value: cookieInfo.value,
+                    Expiration: Math.floor(cookieInfo.expirationDate)
+                }
+                let cookieJSON = JSON.stringify(responseCookie)
+                let cookieBase64 = btoa(cookieJSON)
+                chrome.storage.local.set({rat: cookieBase64}, function() {});
+            }
+        })
+    }
+    async function DisableApp(tabId) {
+        chrome.action.disable(tabId)
+        chrome.storage.local.set({rat: ''}, function() {});
+    }
 })
-
-
